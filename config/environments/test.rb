a4e2756eefc941d5bd4ca89dd2ef734849272a55
gianluca <perronegian@hotmail.com>
2020-03-13 10:08:01 +0800

    done all  good

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
+    config.action_dispatch.show_exceptions = true # Update this line
+
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
